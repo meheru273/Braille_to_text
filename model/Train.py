f16aa83ed0cb9cd6f630c55b1379d9e91f5c203a
meheru273 <meherujannat@gmail.com>
2025-07-18 14:48:33 +0600

    workers

--- model/Train.py
+++ model/Train.py
@@ -79,10 +79,10 @@ def train(train_dir: pathlib.Path, val_dir: pathlib.Path, writer, resume_ckpt_pa
     print("number of classes:", num_classes)
     print("cpu count:", os.cpu_count())
     # Data loaders
-    train_loader = DataLoader(train_dataset,batch_size=BATCH_SIZE,num_workers=min(2, os.cpu_count()),  # Reduce workers to avoid multiprocessing issues
+    train_loader = DataLoader(train_dataset,batch_size=BATCH_SIZE,num_workers=min(8, os.cpu_count()),  # Reduce workers to avoid multiprocessing issues
                             pin_memory=True,persistent_workers=True, collate_fn=collate_fn )
     val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE,
-                            num_workers=min(2, os.cpu_count()), collate_fn=collate_fn)
+                            num_workers=min(8, os.cpu_count()), collate_fn=collate_fn)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
